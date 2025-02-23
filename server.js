d8c4132bea63566805ddddf6c11548d9c9f1ad4b
daniel <danielortizd7mail.com>
2025-02-23 18:42:10 -0500

    Instalado axios para resolver error de Render85

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const conectarDB = require("./app/database/databasemongo");
 const resultadoRoutes = require("./app/routes/resultadoRoutes");
+const cors = require("cors");
 
 dotenv.config();
 conectarDB();
 
-
 const app = express();
-app.use(express.json()); // Middleware para manejar JSON
 
+// 🔹 Habilitar CORS para permitir solicitudes desde el frontend
+app.use(cors({ origin: "*" }));  // ✅ Agregado para evitar el error de CORS
+
+app.use(express.json()); // Middleware para manejar JSON
 app.use("/api", resultadoRoutes); // Prefijo para todas las rutas de resultados
 
 const PORT = process.env.PORT || 5000;
